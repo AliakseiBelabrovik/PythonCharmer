7b2513a07f526d923c9ac6497a2918c12f75c07b
Martin2696 <mato.siska@gmail.com>
2022-06-14 18:25:51 +0200

    added Questions and Description to topics

--- app/src/main/java/com/example/pythoncharmer/models/Question.kt
+++ app/src/main/java/com/example/pythoncharmer/models/Question.kt
@@ -22,11 +22,11 @@ data class Question(
 val questionsTopicComments = listOf(
     Question(
         0,
-        "How do you insert COMMENTS in Python code?",
+        "How does a single line comment look like?",
         listOf<Answer>(
-            Answer("#This is a comment", 0),
-            Answer("//This is a comment", 1),
-            Answer("/*This is a comment*/", 2),
+            Answer("# This is a comment", 0),
+            Answer("// This is a comment", 1),
+            Answer("/* This is a comment */", 2),
             Answer("<!--This is a comment-->", 3),
         ),
         listOf(0),
@@ -35,11 +35,11 @@ val questionsTopicComments = listOf(
     ),
     Question(
     1,
-    "What is correctly commented?",
+    "How does One-line docstring look like?",
         listOf<Answer>(
             Answer("<!--print(\"Hello World!\")-->", 0),
-            Answer("//print(\"Hello World!\")", 1),
-            Answer("\"\"\"print(\"Hello World!\")\"\"\"", 2),
+            Answer("// print(\"Hello World!\")", 1),
+            Answer("\"\"\" print(\"Hello World!\") \"\"\"", 2),
             Answer("<!--This is a comment-->", 3)
         ),
         listOf(2),
@@ -50,26 +50,27 @@ val questionsTopicComments = listOf(
     2,
     "How do you insert a multiline comment?",
         listOf<Answer>(
-            Answer("\"\"\"myName = \"Neo\"\nmy_age = 37\"\"\"", 0),
-            Answer("/*myName = \"Neo\"\nmy_age = 37*/", 1),
-            Answer("###myName = \"Neo\"\nmy_age = 37###", 2),
-            Answer("#myName = \"Neo\"\n#my_age = 37", 3)
+            Answer("\"\"\"\n myName = \"Neo\"\nmy_age = 37 \n\"\"\"", 0),
+            Answer("/* myName = \"Neo\"\nmy_age = 37 */", 1),
+            Answer("### myName = \"Neo\"\nmy_age = 37 ###", 2),
+            Answer("# myName = \"Neo\"\n# my_age = 37", 3)
         ),
-        listOf(0),
-        QuestionType.singleChoice,
+        listOf(0,3),
+        QuestionType.multipleChoice,
     1
     ),
     Question(
     3,
-    "What is true?",
+    "Choose all statements that are True.",
         listOf<Answer>(
-            Answer("Comments are non-executable statements in Python.", 0),
-            Answer("bla bla", 1),
-            Answer("some answer", 2),
-            Answer("what?", 3)
-        ),
-        listOf(0),
-        QuestionType.singleChoice,
+            Answer("Long comments provide more information and are therefore preferred", 0),
+            Answer("Comments are non-executable statements in Python.", 1),
+            Answer("You need as many comments as possible, even if they are redundant", 2),
+            Answer("Comments should describe, how the function does what it does", 3),
+            Answer("Make comments as short and informative as possible.", 4),
+            ),
+        listOf(1,4),
+        QuestionType.multipleChoice,
         1
     )
 )
@@ -105,16 +106,39 @@ val questionsTopicVariables = listOf(
         2,
         "Which of the following statements assigns the value 100 to the variable x in Python:",
         listOf<Answer>(
-            Answer("x = 100", 0),
-            Answer("x << 100", 1),
-            Answer("let x = 100", 2),
-            Answer("x := 100", 3)
-
+            Answer("x << 100", 0),
+            Answer("let x = 100", 1),
+            Answer("x = 100", 2),
+            Answer("x := 100", 3),
+            Answer("x <- 100", 4)
         ),
         listOf(0),
         QuestionType.singleChoice,
         2
-    )
+    ),
+    Question(
+        3,
+        "In Python, a variable may be assigned a value of one type, and then later assigned a value of a different type:",
+        listOf<Answer>(
+            Answer("True", 0),
+            Answer("False", 1)
+        ),
+        listOf(0),
+        QuestionType.singleChoice,
+        2
+    ),
+    Question(
+        4,
+        "Which of the following styles does PEP8 recommend for multi-word variable names:",
+        listOf<Answer>(
+            Answer("DistanceToNearestTown (Pascal Case)", 0),
+            Answer("distanceToNearestTown (Camel Case)", 1),
+            Answer("distance_to_nearest_town (Snake Case)", 2),
+            ),
+        listOf(2),
+        QuestionType.singleChoice,
+        2
+    ),
 )
 
 val allQuestions = questionsTopicComments + questionsTopicVariables
--- app/src/main/java/com/example/pythoncharmer/models/Topics.kt
+++ app/src/main/java/com/example/pythoncharmer/models/Topics.kt
@@ -46,11 +46,27 @@ fun getTopics() : List<Topic> {
             id = 1,
             title = "Comments",
             complexityLevel = "easy",
-            description = "Comments can be used to explain Python code.\n" +
-                    "\n" +
-                    "Comments can be used to make the code more readable.\n" +
-                    "\n" +
-                    "Comments can be used to prevent execution when testing code.",
+            description = "Comments can be used to explain Python code."+"\n"+" Comments can be used to make the code more readable."
+                    +" Comments can be used to prevent execution when testing code."
+                    + "\n\n"
+                    + "A block comment explains the code that follows it. Typically, you indent a block comment at the same level as the code block.\n"
+                    + "To create a block comment, you start with a single hash sign (#) followed by a single space and a text string. "
+                    + "\n\n"
+                    + "A documentation string is a string literal that you put as the first lines in a code block, for example, a function."
+                    + " Unlike a regular comment, a documentation string can be accessed at run-time using  obj.__doc__ attribute where obj is the name of the function."
+                    + " Typically, you use a documentation string to automatically generate the code documentation."
+                    + "\n\n"
+                    + "In a similar way, we can use multiline strings (triple quotes) to write multiline comments."
+                    + "The quotation character can either be ' or \"."
+                    + "\n\n"
+                    + "Python doesn't offer a separate way to write multiline comments. However, there are other ways to get around this issue. "
+                    + "We can use # at the beginning of each line of comment on multiple lines."
+                    + "\n\n"
+                    + "How to Write Better Comments?"
+                    + "\n\n"
+                    + "Use comments to describe what a function does and not the specific details on how the function does it."
+                    + " Try to remove as many redundant comments as possible. Try writing code that can explain itself, using better function/variable name choice."
+                    + " Try to make the comments as short and concise as possible.",
             studyLinks =listOf("https://www.w3schools.com/python/python_comments.asp",
                                "https://www.pythontutorial.net/python-basics/python-comments/"),
             questions = listOf()
@@ -59,7 +75,23 @@ fun getTopics() : List<Topic> {
             id = 2,
             title = "Variables",
             complexityLevel = "easy",
-            description = "A Python variable is a reserved memory location to store values. In other words, a variable in a python program gives data to the computer for processing.\n",
+            description = "A Python variable is a reserved memory location to store values. In other words, a variable in a python program gives data to the computer for processing.\n"
+                    + "\n\n"
+                    + "var1.4 is not valid because it contains a character which is not a letter, digit or underscore.\n"
+                    + "return is not valid because it is a Python reserved word (keyword).\n"
+                    + "2var is not valid because it begins with a digit.\n"
+                    + "\n\n"
+                    + "Variables need not be declared or defined in advance in Python. To create a variable, you just assign it a value."
+                    + "\n\n"
+                    + " A variable is a string of characters and numbers associated with a piece of information."
+                    + " The assignment operator, denoted by the “=” symbol, is the operator that is used to assign values to variables in Python."
+                    + " The line x=1 takes the known value, 1, and assigns that value to the variable with name “x”. After executing this line, this number will be stored into this variable."
+                    + " Until the value is changed or the variable deleted, the character x behaves like the value 1."
+                    + "\n\n"
+                    + " Variables are not statically typed in Python, as they are in some other programming languages."
+                    + "\n\n"
+                    + "Use a lowercase single letter, word, or words. Separate words with underscores to improve readability."
+                    + "\n\n",
             studyLinks =listOf("https://www.w3schools.com/python/python_variables.asp",
                                "https://www.pythontutorial.net/python-basics/python-variables/"),
             questions = listOf(),
