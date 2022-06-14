b687b85502f5edfe721344da6745980e719937f4
Martin2696 <mato.siska@gmail.com>
2022-06-14 20:17:14 +0200

    small changes

--- app/src/main/java/com/example/pythoncharmer/screens/TestScreen.kt
+++ app/src/main/java/com/example/pythoncharmer/screens/TestScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
